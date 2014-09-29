e073984617cbb986b729776c4102a1c8a884e74d
Alex Banks <alex@moonhighway.com>
2014-09-29 15:53:55 -0700

    Adds local data server and data files

---
+++ data-server/data-server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var routes = require('./routes');
+var cors = require('cors')
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'data')));
+
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
+    console.log('D3 Data Server listening on port ' + server.address().port);
+});
---
+++ data-server/readme.md
@@ -0,0 +1,30 @@
+D3 Data File Server
+====================
+This is a node js express server used for serving data files .
+
+Installation
+------------
+1. Install the Dependencies
+
+        $ npm install
+        
+2. Start the Server
+
+        $ npm start
+        
+3. The server will be running on [http://localhost:3000](http://localhost:3000)
+
+
+Data Files
+----------
+* Panorama Pines Golf Course Data
+    * [json](http://localhost:3000/course.json)
+    * [csv](http://localhost:3000/course.csv)
+    * [tsv](http://localhost:3000/course.tsv)
+* Scorecard from Panoram Pines
+    * [json](http://localhost:3000/scores.json)
+    * [csv](http://localhost:3000/scores.csv)
+    * [tsv](http://localhost:3000/scores.tsv)
+* [Ski Resort Info](http://localhost:3000/resorts.json)
+* [Skier Race Times](http://localhost:3000/racetimes.json)
+* [Timeline](http://localhost:3000/timeline.csv)
