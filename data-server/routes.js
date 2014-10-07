804793b1fef79f9102cba563781382f4549dbbdd
Alex Banks <alex@moonhighway.com>
2014-10-06 21:05:38 -0600

    Fixes content type text/plain for home page

--- data-server/routes.js
+++ data-server/routes.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.header('Content-Type', 'text/html; charset=utf-8');
   res.render('index', { title: 'D3 Data' });
 });
 
